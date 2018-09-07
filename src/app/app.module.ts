397fe67cae06ed61de11b13b64a71b2b568851b0
Hoang Son Nguyen <eragonwien@gmail.com>
2018-09-07 06:57:54 +0200

    Add home, about, contact and services components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent, data: { title: 'Startseite' }},
+  { path: 'services', component: ServicesComponent, data: { title: 'Behandlungen' }},
+  { path: 'about', component: AboutComponent, data: { title: 'Über uns' }},
+  { path: 'contact', component: ContactComponent, data: { title: 'Kontakt' }},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent, data: { title: 'Seite nicht gefunden' }}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
