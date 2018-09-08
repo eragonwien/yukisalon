b30e996691b404e68a30bd849c259f98925ec8d3
Hoang Son Nguyen <eragonwien@gmail.com>
2018-09-08 08:51:33 +0200

    Add core routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,25 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ServicesComponent } from './services/services.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductsComponent } from './products/products.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroImgComponent } from './_partials/hero-img/hero-img.component';
+import { ContactInfoComponent } from './_partials/contact-info/contact-info.component';
+import { ContactMapComponent } from './_partials/contact-map/contact-map.component';
+import { ContactOpenHoursComponent } from './_partials/contact-open-hours/contact-open-hours.component';
+import { WelcomeComponent } from './_partials/welcome/welcome.component';
+import { FeaturedComponent } from './_partials/featured/featured.component';
+import { InlineGalleryComponent } from './_partials/inline-gallery/inline-gallery.component';
+import { ProductCardComponent } from './_partials/product-card/product-card.component';
+import { ProductSectionComponent } from './_partials/product-section/product-section.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent, data: { title: 'Startseite' }},
-  { path: 'services', component: ServicesComponent, data: { title: 'Behandlungen' }},
+  { path: 'products', component: ProductsComponent, data: { title: 'Unsere Produkte' }},
   { path: 'about', component: AboutComponent, data: { title: 'Über uns' }},
   { path: 'contact', component: ContactComponent, data: { title: 'Kontakt' }},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -22,10 +33,21 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
-    ServicesComponent,
     AboutComponent,
     ContactComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ProductsComponent,
+    NavigationComponent,
+    FooterComponent,
+    HeroImgComponent,
+    ContactInfoComponent,
+    ContactMapComponent,
+    ContactOpenHoursComponent,
+    WelcomeComponent,
+    FeaturedComponent,
+    InlineGalleryComponent,
+    ProductCardComponent,
+    ProductSectionComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ServicesComponent } from './services.component';
+import { FooterComponent } from './footer.component';
 
-describe('ServicesComponent', () => {
-  let component: ServicesComponent;
-  let fixture: ComponentFixture<ServicesComponent>;
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ServicesComponent ]
+      declarations: [ FooterComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ServicesComponent);
+    fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
