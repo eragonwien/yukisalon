92fc3a4e94c8658a1677c13c1a1efe0a4bb042c7
Hoang Son Nguyen <eragonwien@gmail.com>
2018-09-23 15:36:26 +0200

    Add price list

--- src/app/_partials/price-list/price-list.component.ts
+++ src/app/_partials/price-list/price-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Category } from '../../../models/category';
 
 @Component({
   selector: 'app-price-list',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriceListComponent implements OnInit {
 
+  @Input() categories: Category[];
+  @Input() title: string;
+
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
