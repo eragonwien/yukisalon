02e8b9448a8aab90e7a93e10d5bb9eb8020d8021
Hoang Son Nguyen <eragonwien@gmail.com>
2018-09-30 00:04:20 +0200

    add default banner image

--- src/app/_partials/hero-img/hero-img.component.ts
+++ src/app/_partials/hero-img/hero-img.component.ts
@@ -11,6 +11,7 @@ export class HeroImgComponent implements OnInit {
   @Input() titleText: String;
   @Input() imgName: String;
   bgStyle: any = {}
+  defaultImg: string = "banner2.jpg";
 
   constructor(private service: MainService) { }
 
@@ -19,9 +20,12 @@ export class HeroImgComponent implements OnInit {
   }
 
   getBgStyle(){
-    if (this.imgName) {
-      this.bgStyle["background-image"]=`url('assets/images/${this.imgName}')`;
+    if (!this.imgName) {
+      this.imgName = this.defaultImg;
     }
+    this.bgStyle["background-image"]=`linear-gradient(to top left,rgba(255,192,203, 0.60), rgba(255,192,203, 0.90)), url('assets/images/${this.imgName}')`;
+    this.bgStyle["background-position"]='center'
+
   }
 }
 
