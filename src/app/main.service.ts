f7cbd126be7a75d1a470acfd124b2cde9dcc7bbc
Hoang Son Nguyen <eragonwien@gmail.com>
2018-10-05 08:23:14 +0200

    remove unused functions

--- src/app/main.service.ts
+++ src/app/main.service.ts
@@ -17,64 +17,6 @@ export class MainService {
     return this.http.get<Salon>("assets/data/data.json");
   }
 
-  getFeatures(categories: Category[], maxFeaturesCount: number): Product[] {
-    let products: Product[] = this.getProducts(categories);
-    return this.getFeaturedProducts(products, maxFeaturesCount);
-  }
-
-  getProducts(categories: Category[]): Product[] {
-    let products: Product[] = [];
-
-    for (const category of categories) {
-      let catProducts: Product[] = this.getProductsOfCategory(category);
-      for (const product of catProducts) {
-        products.push(product);
-      }
-    }
-    return products;
-  }
-
-  getProductsOfCategory(category: Category): Product[] {
-    let products: Product[] = [];
-
-    if (!category.subcategories) {
-      return products;
-    }
-
-    for (const subCategory of category.subcategories) {
-      let subCatProducts: Product[] = this.getProductsOfSubCategory(subCategory);
-      for (const product of subCatProducts) {
-        products.push(product);
-      }
-    }
-    return products;
-  }
-
-  getProductsOfSubCategory(subCategory: Subcategory): Product[] {
-    let products: Product[] = [];
-
-    if (!subCategory.products) {
-      return products;
-    }
-
-    for (const product of subCategory.products) {
-      products.push(product);
-    }
-    return products;
-  }
-
-  getFeaturedProducts(products: Product[], maxFeaturesCount: number): Product[] {
-    let features: Product[] = [];
-    let currentCount: number = 0;
-    for (let i = 0; i < products.length && maxFeaturesCount > currentCount; i++) {
-      if (products[i].isFeatured) {
-        features.push(products[i]);
-        currentCount++;
-      }
-    }
-    return features;
-  }
-
   getImagePathPrefix() {
     return this.imagePathPrefix;
   }
