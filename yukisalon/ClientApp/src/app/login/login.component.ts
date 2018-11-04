940b54565e46867aebd97581c838384dd3d33e0a
Hoang Son Nguyen <eragonwien@gmail.com>
2018-11-04 12:26:53 +0100

    update login component

--- yukisalon/ClientApp/src/app/login/login.component.ts
+++ yukisalon/ClientApp/src/app/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginUser } from '../models/User';
+import { AccountService } from '../services/account.service';
+import { SalonService } from '../services/salon.service';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [AccountService, SalonService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginUser: LoginUser;
+
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
+    this.loginUser = new LoginUser();
   }
 
+  onSubmit(loginForm: NgForm) {
+    if (loginForm.valid) {
+      this.accountService.login(this.loginUser).subscribe((response) => {
+        this.handleLogin(response);
+      });
+    } else {
+      alert("Invalid");
+    }
+  }
+
+  handleLogin(response) {
+    this.accountService.Test.subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
