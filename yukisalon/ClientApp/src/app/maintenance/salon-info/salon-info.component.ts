9b62e40c2c6a27e292925f50a1504c767d7dff8c
Hoang Son Nguyen <eragonwien@gmail.com>
2018-11-21 14:11:48 +0100

    handle 404 error

--- yukisalon/ClientApp/src/app/maintenance/salon-info/salon-info.component.ts
+++ yukisalon/ClientApp/src/app/maintenance/salon-info/salon-info.component.ts
@@ -3,6 +3,7 @@ import { SalonService } from '../../services/salon.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Salon } from '../../models/Salon';
 import { NgForm } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-salon-info',
@@ -23,7 +24,7 @@ export class SalonInfoComponent implements OnInit {
     let salonId = this.salonService.currentSalonId;
     this.salonService.getSalonById(salonId).subscribe((salon: Salon) => {
       this.salon = salon;
-    });
+    }, this.handleError);
   }
 
   onSubmit(form: NgForm) {
@@ -36,5 +37,12 @@ export class SalonInfoComponent implements OnInit {
 
   returnToIndex() {
     this.router.navigate(['maintenance']);
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.status === 404) {
+      return this.returnToIndex();
+    }
+    alert(error.message);
   }
 }
