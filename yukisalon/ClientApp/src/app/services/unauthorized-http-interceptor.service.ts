f67c3819f5e8f29b471b42de7d9fcad9da9fce3a
Hoang Son Nguyen <eragonwien@gmail.com>
2018-12-17 14:38:09 +0100

    update to rxjs 6

--- yukisalon/ClientApp/src/app/services/unauthorized-http-interceptor.service.ts
+++ yukisalon/ClientApp/src/app/services/unauthorized-http-interceptor.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import "rxjs/add/operator/catch";
-import 'rxjs/add/observable/throw';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { SalonService } from './salon.service';
 
 @Injectable()
@@ -11,11 +10,12 @@ export class UnauthorizedHttpInterceptorService implements HttpInterceptor {
   constructor(private salonService: SalonService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request).catch(err => {
-      if (err instanceof HttpErrorResponse) {
-        this.salonService.handleHttpError(err);
-      }
-      return Observable.throw(err);
-    });
+    return next.handle(request)
+      .pipe(catchError((err: HttpErrorResponse) => this.handleError(err)));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    this.salonService.handleHttpError(error);
+    return throwError(error);
   }
 }
