5827ef4fa9686c87f8aeec2362594b302282663b
Hoang Son Nguyen <eragonwien@gmail.com>
2018-12-18 22:09:29 +0100

    change alert message to class and has default timeout of 10 sec

--- yukisalon/ClientApp/src/app/models/alertMessage.ts
+++ yukisalon/ClientApp/src/app/models/alertMessage.ts
@@ -1,6 +1,16 @@
-export interface AlertMessage {
-  type: string,
-  header?: string,
-  message: string,
-  timeout?: number
+export class AlertMessage {
+  type: string;
+  message: string;
+  timeout?: number;
+  dismissible: boolean;
+  header?: string;
+
+  constructor(type: string, message: string, timeout: number = 10000, header?: string, dismissible: boolean = true) {
+    this.type = type;
+    this.message = message;
+    this.timeout = timeout;
+    this.header = header;
+    this.dismissible = dismissible;
+  }
+  
 }
