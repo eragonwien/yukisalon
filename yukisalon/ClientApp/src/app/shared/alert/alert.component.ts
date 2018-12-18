34a3d6774786f0099846aaaaaadcdd85c8092dac
Hoang Son Nguyen <eragonwien@gmail.com>
2018-12-18 22:08:25 +0100

    add loader component

---
+++ yukisalon/ClientApp/src/app/shared/alert/alert.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AlertMessage } from 'src/app/models/alertMessage';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.css']
+})
+export class AlertComponent implements OnInit {
+
+  @Input() alerts: AlertMessage[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  close(alert: AlertMessage) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+}
