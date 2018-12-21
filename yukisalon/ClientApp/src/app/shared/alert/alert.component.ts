652a4fb781d8e9de1e0207a372fa0975f191a02c
Hoang Son Nguyen <eragonwien@gmail.com>
2018-12-21 14:13:03 +0100

    fix class not found

--- yukisalon/ClientApp/src/app/shared/alert/alert.component.ts
+++ yukisalon/ClientApp/src/app/shared/alert/alert.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { AlertMessage } from 'src/app/models/alertMessage';
+import { AlertMessage } from '../../models/AlertMessage';
 
 @Component({
   selector: 'app-alert',
