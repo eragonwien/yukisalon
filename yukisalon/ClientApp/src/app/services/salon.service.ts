d0d99a2a5747e64eb1eaa5d7233330672c029c0c
Hoang Son Nguyen <eragonwien@gmail.com>
2018-12-21 14:16:30 +0100

    rearrange code

--- yukisalon/ClientApp/src/app/services/salon.service.ts
+++ yukisalon/ClientApp/src/app/services/salon.service.ts
@@ -1,32 +1,49 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Router } from '@angular/router';
-import { Salon, EditSalonInfoModel } from '../models/Salon';
-import { Contact } from '../models/Contact';
+import { Router } from "@angular/router";
+import { Salon, EditSalonInfoModel } from "../models/Salon";
+import { Contact } from "../models/Contact";
 
 @Injectable()
 export class SalonService {
-
   private currentId: number;
 
-  constructor(private http: HttpClient, private router: Router) { }
+  alertDefaultTimeout: number = 20 * 1000;
+  alertDismissible: boolean = true;
+  alertDefaultSuccessHeader: string = "Erfolgreich";
+  alertDefaultErrorHeader: string = "Fehler";
 
-  get BaseUrl() { return window.location.origin; }
-  get imagePath() { return this.BaseUrl + "/assets/images/"; }
-  get ImagePathPrefix() { return "assets/images/"; }
-  get Salon() { return this.getSalonById(0) }
-  get SalonList() { return this.http.get(this.BaseUrl + "/api/Salon") }
+  openingHourOpenText: string = "Open";
+  openingHourClosedText: string = "Closed";
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  get BaseUrl() {
+    return window.location.origin;
+  }
+  get imagePath() {
+    return this.BaseUrl + "/assets/images/";
+  }
+  get ImagePathPrefix() {
+    return "assets/images/";
+  }
+  get Salon() {
+    return this.getSalonById(0);
+  }
+  get SalonList() {
+    return this.http.get(this.BaseUrl + "/api/Salon");
+  }
   get currentSalonId() {
     if (!this.currentId) {
-      this.currentId = Number(localStorage.getItem('currentSalonId'));
-    } 
+      this.currentId = Number(localStorage.getItem("currentSalonId"));
+    }
     return this.currentId;
   }
 
   set currentSalonId(id: number) {
     if (id) {
       this.currentId = id;
-      localStorage.setItem('currentSalonId', id.toString());
+      localStorage.setItem("currentSalonId", id.toString());
     }
   }
 
@@ -41,38 +58,42 @@ export class SalonService {
   }
 
   handle401Error() {
-    this.router.navigate(['maintenance/login'], { queryParams: { returnUrl: this.router.url }});
+    this.router.navigate(["maintenance/login"], {
+      queryParams: { returnUrl: this.router.url }
+    });
   }
 
   editSalonInfo(salon: Salon) {
     let url = this.BaseUrl + "/api/Salon/" + salon.id;
     let model = new EditSalonInfoModel(salon);
-    return this.http.put(url, model, { observe: 'response' });
+    return this.http.put(url, model, { observe: "response" });
   }
 
   editSalonContact(contact: Contact) {
     let url = this.BaseUrl + "/api/Contact/" + contact.id;
-    return this.http.put(url, contact, { observe: 'response' });
+    return this.http.put(url, contact, { observe: "response" });
   }
 
   createSalonContact(contact: Contact) {
     let url = this.BaseUrl + "/api/Contact";
-    return this.http.post(url, contact, { observe: 'response' });
+    return this.http.post(url, contact, { observe: "response" });
   }
 
   removeSalonContact(contact: Contact) {
     let url = this.BaseUrl + "/api/Contact/" + contact.id;
-    return this.http.delete(url, { observe: 'response' });
+    return this.http.delete(url, { observe: "response" });
   }
 
-  returnToMaintenanceIndex(){
-    return this.router.navigate(['maintenance']);
+  returnToMaintenanceIndex() {
+    return this.router.navigate(["maintenance"]);
   }
 
   scrollToViewById(divId: string) {
     let targetDiv = document.getElementById(divId);
     if (targetDiv) {
-      document.getElementById(divId).scrollIntoView({ behavior: 'smooth', block: 'start' });
+      document
+        .getElementById(divId)
+        .scrollIntoView({ behavior: "smooth", block: "start" });
     }
   }
 }
