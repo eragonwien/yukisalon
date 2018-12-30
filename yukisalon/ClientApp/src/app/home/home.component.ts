4ea147669a3f4f816fd16f74233a9be10afbebff
Hoang Son Nguyen <eragonwien@gmail.com>
2018-12-30 13:24:46 +0100

    remove unused import

--- yukisalon/ClientApp/src/app/home/home.component.ts
+++ yukisalon/ClientApp/src/app/home/home.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { SalonService } from "../services/salon.service";
-import { Welcome } from "../models/Welcome"
-import { Salon } from "../models/Salon"
-import { Contact } from '../models/Contact';
-import { Category } from '../models/Product';
+import { Welcome } from "../models/Welcome";
+import { Salon } from "../models/Salon";
+import { Contact } from "../models/Contact";
+import { Category } from "../models/Product";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+  selector: "app-home",
+  templateUrl: "./home.component.html"
 })
 export class HomeComponent {
-
   title: string = "Yuki Tuyet Beauty Salon";
   welcome: Welcome;
   contacts: Contact[];
