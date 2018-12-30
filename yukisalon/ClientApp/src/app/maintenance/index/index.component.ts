253158c478d65c64a068d8862bc250add26636ed
Hoang Son Nguyen <eragonwien@gmail.com>
2018-12-30 13:22:53 +0100

    check user login on load

--- yukisalon/ClientApp/src/app/maintenance/index/index.component.ts
+++ yukisalon/ClientApp/src/app/maintenance/index/index.component.ts
@@ -1,32 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { Salon } from '../../models/Salon';
-import { SalonService } from '../../services/salon.service';
-import { Router } from '@angular/router';
-import { MaintenanceMenuOption as MaintenanceMenuOption } from '../../models/maintenance.menu';
-import { AccountService } from '../../services/account.service';
+import { Component, OnInit } from "@angular/core";
+import { Salon } from "../../models/Salon";
+import { SalonService } from "../../services/salon.service";
+import { Router } from "@angular/router";
+import { AccountService } from "../../services/account.service";
 
 @Component({
-  selector: 'app-maintenance-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  selector: "app-maintenance-index",
+  templateUrl: "./index.component.html",
+  styleUrls: ["./index.component.css"]
 })
 export class MaintenanceIndexComponent implements OnInit {
-
   salonList: Salon[];
   salon: Salon;
   quickMenu: boolean = false; // if true, then pick a salon if it is the only one
 
-  constructor(private salonService: SalonService, private router: Router, private accountService: AccountService) { }
+  constructor(
+    private salonService: SalonService,
+    private router: Router,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit() {
-    this.resetPickedSalon();
-    this.loadSalonList();
+    this.accountService
+      .sendTestAuth()
+      .subscribe()
+      .add(() => {
+        this.resetPickedSalon();
+        this.loadSalonList();
+      });
   }
 
   loadSalonList() {
     this.salonService.SalonList.subscribe((results: Salon[]) => {
       this.salonList = results;
-        if (this.quickMenu && this.salonList && this.salonList.length === 1) {
+      if (this.quickMenu && this.salonList && this.salonList.length === 1) {
         return this.onSalonPicked(this.salonList[0]);
       }
     });
@@ -34,7 +41,7 @@ export class MaintenanceIndexComponent implements OnInit {
 
   onSalonPicked(salon: Salon) {
     this.salonService.currentSalonId = salon.id;
-    this.router.navigate(['maintenance/menu']);
+    this.router.navigate(["maintenance/menu"]);
   }
 
   resetPickedSalon() {
