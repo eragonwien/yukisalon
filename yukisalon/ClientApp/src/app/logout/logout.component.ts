b54fe442789808f98d5acdab47a05d9a794aa962
Hoang Son Nguyen <eragonwien@gmail.com>
2018-12-30 13:24:16 +0100

    fix logout not called

--- yukisalon/ClientApp/src/app/logout/logout.component.ts
+++ yukisalon/ClientApp/src/app/logout/logout.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../services/account.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AccountService } from "../services/account.service";
 
 @Component({
-  selector: 'app-logout',
-  templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+  selector: "app-logout",
+  templateUrl: "./logout.component.html",
+  styleUrls: ["./logout.component.css"]
 })
 export class LogoutComponent implements OnInit {
-
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.accountService.logout();
-    return this.accountService.redirectToLogin();
+    this.accountService
+      .logout()
+      .subscribe()
+      .add(() => this.accountService.redirectToLogin());
   }
-
 }
