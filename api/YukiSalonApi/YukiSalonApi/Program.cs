4b4beb1ae6aa04546e45e7e5fe3d02cff5e15e0f
Hoang Son Nguyen <eragonwien@gmail.com>
2019-01-26 21:38:22 +0100

    fix import

--- api/YukiSalonApi/YukiSalonApi/Program.cs
+++ api/YukiSalonApi/YukiSalonApi/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using yukisalon.Models;
 using YukiSalonApi.Models;
 
 namespace YukiSalonApi
