172e07105ed21de38626932784431ffd71475565
Hoang Son Nguyen <eragonwien@gmail.com>
2019-01-26 21:39:52 +0100

    add unit test for hashing password

---
+++ api/YukiSalonApi/XUnitTestApi/SalonControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Xunit;
+using YukiSalonApi.Controllers;
+using YukiSalonApi.Models;
+
+namespace XUnitTestApi
+{
+    public class SalonControllerTest
+    {
+        private readonly SalonController controller;
+        private readonly YUKISALONDEVContext context;
+
+        public SalonControllerTest()
+        {
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+
+        }
+    }
+}
---
+++ api/YukiSalonApi/XUnitTestProject/SalonServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using YukiSalonApi;
+
+namespace XUnitTestProject
+{
+    public class SalonServiceTest
+    {
+        private readonly SalonService service;
+
+        public SalonServiceTest()
+        {
+            service = new SalonService();
+        }
+
+        [Fact]
+        public void ReturnTrue_GivenCorrectPassword()
+        {
+            // Arrange
+            const string password = "passowrd";
+            string hashedPassword = service.GetEncodedPassword(password);
+
+            // Act
+            bool result = service.IsPasswordValid(password, hashedPassword);
+
+            // Assert
+            Assert.True(result, "Password should be valid");
+        }
+
+        [Fact]
+        public void ReturnFalse_GivenWrongPassword()
+        {
+            // Arrange
+            const string correctPassword = "passowrd";
+            const string wrongPassword = "Password";
+            string hashedCorrectPassword = service.GetEncodedPassword(correctPassword);
+
+            // Act
+            bool result = service.IsPasswordValid(wrongPassword, hashedCorrectPassword);
+
+            // Assert
+            Assert.False(result, "Password should not be valid");
+        }
+    }
+}
