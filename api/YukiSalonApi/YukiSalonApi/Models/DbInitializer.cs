156606208a652effa89f4e92d2d124155032fc65
Hoang Son Nguyen <eragonwien@gmail.com>
2019-01-29 17:45:23 +0100

    change scope

--- api/YukiSalonApi/YukiSalonApi/Models/DbInitializer.cs
+++ api/YukiSalonApi/YukiSalonApi/Models/DbInitializer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using YukiSalonApi;
+using YukiSalonApi.Services;
 using YukiSalonApi.Models;
 
 namespace YukiSalonApi.Models
