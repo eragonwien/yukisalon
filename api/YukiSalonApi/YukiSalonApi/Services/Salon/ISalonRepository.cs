dca8e4e5df475fbbe4c95f8fd3f93efe22e599c8
Hoang Son Nguyen <eragonwien@gmail.com>
2019-02-02 11:02:42 +0100

    add salon repository

---
+++ api/YukiSalonApi/YukiSalonApi/Services/Salon/ISalonRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using YukiSalonApi.Models;
+
+namespace YukiSalonApi.Services
+{
+    public interface ISalonRepository
+    {
+        Task<List<Salon>> GetAll();
+        Salon GetOne(int id);
+        int GetFirstId();
+        void Add(Salon salon);
+        void Update(Salon salon);
+        void Remove(int id);
+        Task SaveChanges();
+        bool Exist(int id);
+        Task<List<Category>> GetSubcategories(int id);
+    }
+}
