081fce5693485124a9cb80779712cbff43d3d026
Hoang Son Nguyen <eragonwien@gmail.com>
2019-02-02 11:04:47 +0100

    add contact repo

---
+++ api/YukiSalonApi/YukiSalonApi/Services/Contact/IContactRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YukiSalonApi.Models;
+
+namespace YukiSalonApi.Services
+{
+    public interface IContactRepository
+    {
+        Task<List<Contact>> GetAll();
+        Task<Contact> GetOne(int id);
+        void Add(Contact contact);
+        void Update(Contact contact);
+        string Remove(int id);
+        Task SaveChanges();
+        bool Exist(int id);
+        bool Exist(Contact contact);
+    }
+}
