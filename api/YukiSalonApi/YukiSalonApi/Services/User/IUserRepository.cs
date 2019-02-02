45972e6ec4abbda26fd8c442f120441a3869c30b
Hoang Son Nguyen <eragonwien@gmail.com>
2019-02-02 11:02:32 +0100

    add user repository

---
+++ api/YukiSalonApi/YukiSalonApi/Services/User/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YukiSalonApi.Models;
+
+namespace YukiSalonApi.Services
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAll();
+        Task<User> GetOne(int id);
+        void Add(User user);
+        void Update(User user);
+        string Remove(int id, string login);
+        Task SaveChanges();
+        bool Exist(int id);
+        bool Exist(User user);
+    }
+}
---
+++ api/YukiSalonApi/YukiSalonApi/Services/User/UserRepository.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YukiSalonApi.Models;
+
+namespace YukiSalonApi.Services
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly YUKISALONDEVContext context;
+
+        public UserRepository(YUKISALONDEVContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(User user)
+        {
+            if (!Exist(user))
+            {
+                throw new Exception("Email already exists");
+            }
+
+            if (user.RoleId < 0)
+            {
+                user.RoleId = context.Role.First().Id;
+            }
+
+            context.User.Add(user);
+        }
+
+        public bool Exist(int id)
+        {
+            return context.User.Any(u => u.Id == id);
+        }
+
+        public bool Exist(User user)
+        {
+            return context.User.Any(u => u.Email.Equals(user.Email));
+        }
+
+        public Task<List<User>> GetAll()
+        {
+            return context.User.ToListAsync();
+        }
+
+        public Task<User> GetOne(int id)
+        {
+            return context.User.Where(u => u.Id == id).SingleAsync();
+        }
+
+        public string Remove(int id, string login)
+        {
+            User removeUser = context.User.SingleOrDefault(u => u.Id == id);
+
+            if (removeUser == null)
+            {
+                return string.Empty;
+            }
+
+            if (removeUser.Email.Equals(login))
+            {
+                return "cannot remove self";
+            }
+
+            if (context.User.Count(u => u.SalonId == removeUser.SalonId) == 1)
+            {
+                return "cannot remove last User of Salon " + removeUser.Salon.Name;
+            }
+
+            return string.Empty;
+        }
+
+        public Task SaveChanges()
+        {
+            return context.SaveChangesAsync();
+        }
+
+        public void Update(User user)
+        {
+            context.Update(user);
+        }
+    }
+}
