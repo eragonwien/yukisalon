84c37d412eb0a612719571d2b46604e72ad7aeaa
Hoang Son Nguyen <eragonwien@gmail.com>
2019-02-03 09:21:49 +0100

    store constants in static class

---
+++ api/YukiSalonApi/YukiSalonApi/Services/Constant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YukiSalonApi.Services
+{
+    public static class Constant
+    {
+        public const string ALLOWED_ORIGINS = "";
+        public const int COOKIE_MAX_AGE_MINUTE = 20;
+        public const string LOGIN_CLAIM_IDENTITY = "ClamSheell";
+        public const string IMAGES_DIRECTORY = "images";
+    }
+}
