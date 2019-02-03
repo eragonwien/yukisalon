7b5c39c31087cf727486952aa76873d1941173da
Hoang Son Nguyen <eragonwien@gmail.com>
2019-02-03 09:16:23 +0100

    add extension test, add test for validating mimetype

---
+++ api/YukiSalonApi/XUnitTestProject/ExtensionTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using YukiSalonApi.Services;
+
+namespace XUnitTestProject
+{
+    public class ExtensionTest
+    {
+        [Fact]
+        public void ReturnExtension_GivenValidRegularFileName()
+        {
+            // Arrange
+            string filename = "test.pdf";
+            string expected = "pdf";
+
+            // Act 
+            string result = filename.GetFileExtension();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ReturnExtension_GivenValidMultiPointFileName()
+        {
+            // Arrange
+            string filename = "test.test.pdf";
+            string expected = "pdf";
+
+            // Act 
+            string result = filename.GetFileExtension();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ReturnEmpty_GivenEmptyFileName()
+        {
+            // Arrange
+            string filename = string.Empty;
+            string expected = string.Empty;
+
+            // Act 
+            string result = filename.GetFileExtension();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ReturnEmpty_GivenFileNameWithoutDot()
+        {
+            // Arrange
+            string filename = "test";
+            string expected = string.Empty;
+
+            // Act 
+            string result = filename.GetFileExtension();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
--- api/YukiSalonApi/XUnitTestProject/SalonServiceTest.cs
+++ api/YukiSalonApi/XUnitTestProject/SalonServiceTest.cs
@@ -1,5 +1,8 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
+using YukiSalonApi.Models;
 using YukiSalonApi.Services;
+using HeyRed.Mime;
 
 namespace XUnitTestProject
 {
@@ -39,6 +42,73 @@ namespace XUnitTestProject
 
             // Assert
             Assert.False(result, "Password should not be valid");
+        }
+
+        [Fact]
+        public void ReturnFileName_GivenValidImage()
+        {
+            // Arrange
+            string JPEG = "jpeg";
+            Image image = new Image()
+            {
+                Id = 1,
+                MimeType = MimeTypesMap.GetMimeType(JPEG)
+            };
+            string expected = image.Id + "." + JPEG;
+
+            // Act 
+            string result = service.GetFileName(image);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ThrowException_GivenEmptyImageId()
+        {
+            // Arrange
+            Image image = new Image();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => service.GetFileName(image));
+        }
+
+        [Fact]
+        public void ThrowException_GivenEmptyMimeType()
+        {
+            // Arrange
+            Image image = new Image() { Id = 1 };
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => service.GetFileName(image));
+        }
+
+        [Fact]
+        public void ReturnMimeType_GivenValidFilename()
+        {
+            // Arrange
+            string filename = "test.pdf";
+            string expected = MimeTypesMap.GetMimeType("pdf");
+
+            // Act
+            string result = service.GetMimeType(filename);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ReturnDefaultMimeType_GivenInvalidFilename()
+        {
+            // Arrange
+            string filename = "test.test.test";
+
+            // Act
+            string result = service.GetMimeType(filename);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(MimeTypesMap.GetExtension(result));
         }
     }
 }
