edc9020aee8e2edbde67772d20328b3c7779e204
Hoang Son Nguyen <eragonwien@gmail.com>
2019-02-03 09:22:00 +0100

    add extension class

---
+++ api/YukiSalonApi/YukiSalonApi/Services/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YukiSalonApi.Services
+{
+    public static class Extensions
+    {
+        public static string GetFileExtension(this string filename)
+        {
+            if (string.IsNullOrEmpty(filename) || !filename.Contains('.'))
+            {
+                return string.Empty;
+            }
+
+            return filename.Substring(filename.LastIndexOf('.') + 1);
+        }
+    }
+}
